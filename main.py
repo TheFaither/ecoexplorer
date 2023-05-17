c708b3e07a59376e4e62be7beb81feaa9a454753
Daniele Liprandi <daniele.liprandi@gmail.com>
2023-05-17 10:18:24 +0200

    Add date filter

--- main.py
+++ main.py
@@ -8,26 +8,31 @@ import streamlit as st
 import plotly.express as px
 from sqlalchemy import create_engine
 import os
+import datetime
+import pandas as pd
 
-full_path = os.path.join(".","tempDir")
-os.makedirs(full_path,exist_ok=True)
+full_path = os.path.join(".", "tempDir")
+os.makedirs(full_path, exist_ok=True)
 
 conn = st.experimental_connection("local_db", type="sql", url="sqlite:///")
 engine = create_engine(url="sqlite:///")
 
+
 def save_uploaded_file(uploadedfile):
-    with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
-       f.write(uploadedfile.getbuffer())
+    with open(os.path.join("tempDir", uploadedfile.name), "wb") as f:
+        f.write(uploadedfile.getbuffer())
     return st.sidebar.success("Open file :{} in tempDir".format(uploadedfile.name))
+
 
 datafile = st.sidebar.file_uploader("Upload database")
 if datafile is not None:
-    file_details = {"FileName":datafile.name,"FileType":datafile.type}
+    file_details = {"FileName": datafile.name, "FileType": datafile.type}
     # Apply Function here
     save_uploaded_file(datafile)
     st.cache_data.clear()
-    conn = st.experimental_connection("local_db", type="sql", url="sqlite:///tempDir/"+datafile.name)
-
+    conn = st.experimental_connection(
+        "local_db", type="sql", url="sqlite:///tempDir/" + datafile.name
+    )
 
 
 dfi = conn.query("select * from investigator")
@@ -35,6 +40,9 @@ dfs = conn.query("select * from sample")
 dft1 = conn.query("select * from silktrait NATURAL JOIN trait")
 dft2 = conn.query(
     "select * from individualtrait NATURAL JOIN trait INNER JOIN sample ON sample.id = trait.samples_id"
+)
+dft2[["uploaddate", "collectiondate"]] = dft2[["uploaddate", "collectiondate"]].apply(
+    pd.to_datetime
 )
 dfe = conn.query("select * from experiment")
 dff = [dfi, dfs, dft1, dft2, dfe]
@@ -103,13 +111,24 @@ with tabs[3]:
     st.write("Weight chart")
     # --------------------------------- histogram -------------------------------- #
     optiongroupby = st.selectbox(
-        "Group by", ["family", "genus", "species", "samples_id"], index=0
+        "Color by", ["family", "genus", "species", "samples_id"], index=0
     )
     optionsselect = st.multiselect(
-    'Filter',
-    dfs.nomenclature.unique(), dfs.nomenclature.unique())
+        "Filter", dfs.nomenclature.unique(), dfs.nomenclature.unique()
+    )
+    startdate = st.slider(
+        "Show only data uploaded after this date",
+        min_value=datetime.datetime.fromisocalendar(2020, 1, 1),
+        max_value=datetime.datetime.fromisocalendar(2024, 1, 1),
+        value=datetime.datetime.fromisocalendar(2022, 1, 1),
+    )
 
-    figtraithist = px.histogram(dft2.query('nomenclature in @optionsselect'), x="weight", nbins=20, color=optiongroupby)
+    figtraithist = px.histogram(
+        dft2.query("nomenclature in @optionsselect").query("uploaddate > @startdate"),
+        x="weight",
+        nbins=20,
+        color=optiongroupby,
+    )
     st.plotly_chart(figtraithist)
 # st.experimental_data_editor(
 #     dfs,
