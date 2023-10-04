4e35214659635662dce102a5df129c0bc7b5dd04
Daniele Liprandi <daniele.liprandi@uni-greifswald.de>
2023-10-04 13:45:16 +0200

    add to sql

--- main.py
+++ main.py
@@ -426,7 +426,7 @@ with tabs[7]:
 # ---------------------------------------------------------------------------- #
 with tabs[8]:
     st.data_editor(dfs)
-
+    dfs.to_sql("sample", conn, if_exists="replace")
 #     st.experimental_data_editor(
 #         dft1,
 #         width=None,
